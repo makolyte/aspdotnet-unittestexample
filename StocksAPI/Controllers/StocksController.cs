b26d838abd67c3a2a02c835d28df6dd039392a52
Maclain Wiltzer <mwiltzer@gmail.com>
2021-05-24 08:28:07 -0400

    Init StocksAPI proj with empty controller

---
+++ StocksAPI/Controllers/StocksController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace StocksAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StocksController : ControllerBase
+    {
+        public StocksController()
+        {
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get(string symbol)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ StocksAPI/Models/Stock.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StocksAPI.Models
+{
+    public class Stock
+    {
+        public string Symbol { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public DateTimeOffset QuoteTime { get; set; }
+    }
+}
