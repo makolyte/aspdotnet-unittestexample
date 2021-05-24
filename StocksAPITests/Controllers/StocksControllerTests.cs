f144e881cb4b9eba87ca2e955474f572b7590514
Maclain Wiltzer <mwiltzer@gmail.com>
2021-05-24 08:29:18 -0400

    Added empty unit test project

---
+++ StocksAPITests/Controllers/StocksControllerTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StocksAPI.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StocksAPI.Controllers.Tests
+{
+    [TestClass()]
+    public class StocksControllerTests
+    {
+        [TestMethod()]
+        public void GetTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
